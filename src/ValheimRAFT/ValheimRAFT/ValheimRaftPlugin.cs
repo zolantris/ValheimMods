58cfe308b8c298ca4d5e4844e277e8c0f2b4deb0
zolantris <zolantris@vllc.dev>
2024-01-20 23:28:58 -0800

    bump raft version to 1.6.13

--- src/ValheimRAFT/ValheimRAFT/ValheimRaftPlugin.cs
+++ src/ValheimRAFT/ValheimRAFT/ValheimRaftPlugin.cs
@@ -22,7 +22,7 @@ public class ValheimRaftPlugin : BaseUnityPlugin
    * @note keeping this as Sarcen for now since there are low divergences from the original codebase and patches already mapped to sarcen's mod
    */
   public const string Author = "Sarcen";
-  public const string Version = "1.6.11";
+  public const string Version = "1.6.13";
   internal const string ModName = "ValheimRAFT";
   public const string BepInGuid = $"BepIn.{Author}.{ModName}";
   private const string HarmonyGuid = $"Harmony.{Author}.{ModName}";
