8bfc5d5d4cc38d0ef8ec1d0f58bd87615e7dea2b
zolantris <zolantris@vllc.dev>
2024-01-28 02:31:14 -0800

    attempt to get sentry scopping working

--- src/SentryUnityWrapper/SentryUnityWrapper/SentryUnityWrapperPlugin.cs
+++ src/SentryUnityWrapper/SentryUnityWrapper/SentryUnityWrapperPlugin.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using BepInEx;
+using HarmonyLib;
 using Sentry;
+using Sentry.Extensibility;
 using Sentry.Unity;
+using SentryUnitySdkWrapper;
 using UnityEngine;
 
 namespace SentryUnityWrapper;
@@ -24,6 +28,8 @@ public class SentryUnityWrapperPlugin : BaseUnityPlugin
   public const string CopyRight = "Copyright © 2024, GNU-v3 licensed";
 
   public static Dictionary<string, SentryClient> RegisteredPluginClients = new();
+  public static List<string> RegisteredPluginNames = new();
+  public static Dictionary<string, Config> RegisteredPluginClientOptions = new();
   public static Dictionary<string, Config> PendingPluginsToRegister = new();
 
   private bool _canAutoRegister = true;
@@ -34,6 +40,22 @@ public class SentryUnityWrapperPlugin : BaseUnityPlugin
   {
     StartAutoRegistry();
   }
+
+  // private class BepInExSentryMiddleware
+  // {
+  //   private readonly ISentryClient _client;
+  //   private readonly dynamic? _middleware;
+  //
+  //   private readonly Config _options;
+  //
+  //   public BepInExSentryMiddleware(ISentryClient client, Config options, dynamic? middleware)
+  //   {
+  //     _client = client;
+  //     _options = options;
+  //     _middleware = middleware;
+  //   }
+  // }
+
 
   public void StartAutoRegistry()
   {
@@ -64,6 +86,40 @@ public class SentryUnityWrapperPlugin : BaseUnityPlugin
   {
     var pluginsToRegister = PendingPluginsToRegister.ToList();
     pluginsToRegister.ForEach((plugin) => { InitializeScopedLogging(plugin.Key, plugin.Value); });
+  }
+
+  public static SentryEvent? InvokeForAllClients(SentryEvent @event, string currentPluginName)
+  {
+    if (@event.Exception == null) return null;
+
+    foreach (var pluginName in RegisteredPluginNames)
+    {
+      // we may need to skip the current scoped plugin and delegate that logic somewhere else
+      // if (currentPluginName == pluginName) continue;
+
+      var clientOptions = RegisteredPluginClientOptions.GetValueSafe(pluginName);
+      var client = RegisteredPluginClients.GetValueSafe(pluginName);
+
+      if (client == null || clientOptions == null) continue;
+
+      using (SentrySdk.PushScope())
+      {
+        if (clientOptions.PluginName != "")
+        {
+          SentrySdk.AddBreadcrumb(clientOptions.PluginName, "plugin-name");
+        }
+
+        if (@event.Exception == null ||
+            !@event.Exception.StackTrace.Contains(clientOptions.PluginName)) continue;
+
+        // Within this scope, the current client will be used instead or on top of whatever
+        // client was defined before this point:
+        SentrySdk.BindClient(client);
+        if (@event.Exception != null) SentrySdk.CaptureException(@event.Exception);
+      } // Scope is disposed.
+    }
+
+    return null;
   }
 
   public static Config CreateConfig(string pluginGuid, string modName,
@@ -139,10 +195,10 @@ public class SentryUnityWrapperPlugin : BaseUnityPlugin
         // A Sentry Data Source Name (DSN) is required.
         // See https://docs.sentry.io/product/sentry-basics/dsn-explainer/
         // You can set it in the SENTRY_DSN environment variable, or you can set it in code here.
-        sentryUnityConfig.Dsn = options.GetSentryUnityOptions().Dsn;
+        // sentryUnityConfig.Dsn = options.GetSentryUnityOptions().Dsn;
 
         sentryUnityConfig.CacheDirectoryPath =
-          Path.Combine(Paths.PluginPath, options.PluginName);
+          Path.Combine(Paths.PluginPath, SentryUnityWrapperPlugin.ModName);
 
         // When debug is enabled, the Sentry client will emit detailed debugging information to the console.
         // This might be helpful, or might interfere with the normal operation of your application.
@@ -192,19 +248,40 @@ public class SentryUnityWrapperPlugin : BaseUnityPlugin
       TracesSampleRate = 1.0,
     };
 
-    // scopes based on plugin name being in the stack trace
-    clientOptions.SetBeforeSend(@event =>
-    {
-      if (@event.Exception != null && @event.Exception.StackTrace.Contains(options.PluginName))
-      {
-        ZLog.Log($"StackTrace {@event.Exception.StackTrace}");
-        return @event;
-      }
+    clientOptions.SetBeforeSend(@event => InvokeForAllClients(@event, options.PluginName));
 
-      return null;
-    });
+    // scopes based on plugin name being in the stack trace
 
     var client = new SentryClient(clientOptions);
+
+    // var adminDsn = new Dsn(clientOptions.Dsn);
+    // using (var client = new SentryClient(clientOptions))
+    // {
+    //   var middleware = new BepInExSentryMiddleware(client, options, null);
+    //   clientOptions.SetBeforeSend(@event =>
+    //   {
+    //     if (@event.Exception != null && @event.Exception.StackTrace.Contains(options.PluginName))
+    //     {
+    //       ZLog.Log($"StackTrace {@event.Exception.StackTrace}");
+    //       return @event;
+    //     }
+    //
+    //     middleware.Invoke(@event);
+    //
+    //     return null;
+    //   });
+    //
+    //   // middleware.Invoke(request);
+    // }
+
+    // SentrySdk.BindClient();
+    // SentrySdk.CaptureEvent(evt =>
+    // {
+    //   if (evt)
+    //   {
+    //     return client;
+    //   }
+    // })
     // client.CaptureEvent()
 
 
@@ -214,6 +291,9 @@ public class SentryUnityWrapperPlugin : BaseUnityPlugin
 
 
     PendingPluginsToRegister.Remove(options.PluginGuid);
+    // adds related items to database to delegate sending messages
+    RegisteredPluginNames.Add(options.PluginName);
+    RegisteredPluginClientOptions.Add(options.PluginName, options);
     RegisteredPluginClients.Add(options.PluginGuid, client);
 
     return true;
