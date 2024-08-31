bf3b71df835a7cf0e1ebf49fd19b07627554a01c
lijspoop <lijs.poop@gmail.com>
2024-08-31 20:57:45 +0800

    Update the server and client running configurations
    * Add logValheim.ps1
    * Update gameLauncher.targets
    * Update valheim.targets
    * Update ValheimMods.sln
    * Update .csproj files of ValheimMods.sln
    * Comment out code in SentryMetrics.cs
      > To avoid errors during the build

--- src/ValheimRAFT/ValheimRAFT.Util/SentryMetrics.cs
+++ src/ValheimRAFT/ValheimRAFT.Util/SentryMetrics.cs
@@ -2,7 +2,6 @@ using System;
 using System.Reflection;
 using Jotunn;
 using Properties;
-using SentryUnityWrapper;
 
 namespace ValheimRAFT.Util;
 
@@ -26,18 +25,18 @@ public abstract class SentryMetrics
     // Dsn = Assembly.GetAssembly().GetCustomAttribute<SentryDSN>().Dsn;
 
     // todo to remove this from hardcoded config
-    var Dsn =
-      "https://e720adb5b1a1fdb40d073635eb76817d@o243490.ingest.us.sentry.io/4506613652586496";
-    Logger.LogDebug($"DSN FOR sentry {Dsn}");
-    SentryUnityWrapperPlugin.RegisterPluginAsync(
-      new Config(ValheimRaftPlugin.ModGuid, ValheimRaftPlugin.ModName, ValheimRaftPlugin.Version,
-        Dsn));
-
-
-    var dsn =
-      "https://45e6b5f08cfdf76cae86a36cc3bdffd1@o243490.ingest.us.sentry.io/4506635619467264";
-    SentryUnityWrapperPlugin.RegisterPluginAsync(
-      new Config("zolantris.ValheimVehicles", "ValheimVehicles", ValheimRaftPlugin.Version,
-        dsn));
+    // var Dsn =
+    //   "https://e720adb5b1a1fdb40d073635eb76817d@o243490.ingest.us.sentry.io/4506613652586496";
+    // Logger.LogDebug($"DSN FOR sentry {Dsn}");
+    // SentryUnityWrapperPlugin.RegisterPluginAsync(
+    //   new Config(ValheimRaftPlugin.ModGuid, ValheimRaftPlugin.ModName, ValheimRaftPlugin.Version,
+    //     Dsn));
+    //
+    //
+    // var dsn =
+    //   "https://45e6b5f08cfdf76cae86a36cc3bdffd1@o243490.ingest.us.sentry.io/4506635619467264";
+    // SentryUnityWrapperPlugin.RegisterPluginAsync(
+    //   new Config("zolantris.ValheimVehicles", "ValheimVehicles", ValheimRaftPlugin.Version,
+    //     dsn));
   }
 }
