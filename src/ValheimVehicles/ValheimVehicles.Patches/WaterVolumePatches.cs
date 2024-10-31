e55dd54f63ec81504ac7e575a4976ea3e3bddeaf
zolantris <zolantris@vllc.dev>
2024-10-31 16:36:44 -0700

    Revert "WIP: need to revert changes to water height"
    This reverts commit 03ee0120e5d30c102a0df9c33b97c054aabc96f0.

--- src/ValheimVehicles/ValheimVehicles.Patches/WaterVolumePatches.cs
+++ src/ValheimVehicles/ValheimVehicles.Patches/WaterVolumePatches.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using HarmonyLib;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -66,150 +64,6 @@ namespace ValheimVehicles.Patches
       return true;
     }
 
-    public static bool IsWaveOverridesEnabled =>
-      WaterConfig.FlipWatermeshMode.Value !=
-      WaterConfig.WaterMeshFlipModeType.Disabled &&
-      WaterConfig.UnderwaterAccessMode.Value !=
-      WaterConfig.UnderwaterAccessModeType.Disabled;
-
-    // Patch for GetWaterSurface
-    [HarmonyPatch(typeof(WaterVolume), nameof(WaterVolume.GetWaterSurface))]
-    [HarmonyPrefix]
-    public static bool WaterVolume_GetWaterSurface(WaterVolume __instance,
-      ref float __result, Vector3 point, float waveFactor = 1f)
-    {
-      if (!IsWaveOverridesEnabled) return true;
-
-      var num = 0.0f;
-
-      if (__instance.m_useGlobalWind)
-      {
-        var wrappedDayTimeSeconds = WaterVolume.s_wrappedDayTimeSeconds;
-        var depth = __instance.Depth(point);
-        num = (double)depth == 0.0
-          ? 0.0f
-          : __instance.CalcWave(point, depth, wrappedDayTimeSeconds,
-            waveFactor);
-      }
-
-      // Adjust water surface height based on flip
-      __result = __instance.transform.position.y + (IsFlipped() ? -num : num) +
-                 __instance.m_surfaceOffset;
-
-      // Additional adjustments if necessary
-      if ((double)__instance.m_forceDepth < 0.0 &&
-          (double)Utils.LengthXZ(point) > 10500.0)
-        __result -= 100f;
-
-      return false;
-    }
-
-    public static float[] AboveOceanDepth = new float[4];
-
-    // Patch for CalcWave
-    [HarmonyPatch(typeof(WaterVolume), nameof(WaterVolume.CalcWave),
-      new[]
-      {
-        typeof(Vector3), typeof(float), typeof(Vector4), typeof(float),
-        typeof(float)
-      })]
-    [HarmonyPrefix]
-    public static bool PrefixCalcWave(WaterVolume __instance, Vector3 worldPos,
-      float depth, Vector4 wind, float waterTime, float waveFactor,
-      ref float __result)
-    {
-      if (!IsFlipped())
-      {
-        var oceanDepth = __instance.m_heightmap.GetOceanDepth();
-        AboveOceanDepth = oceanDepth;
-        return true;
-      }
-
-      var reversedDepthDirections = new List<Vector2>();
-
-      reversedDepthDirections.Clear();
-      foreach (var sCreateWaveDirection in WaterVolume.s_createWaveDirections)
-      {
-        reversedDepthDirections.Add(new Vector2(sCreateWaveDirection.x,
-          -sCreateWaveDirection.y));
-      }
-
-      var reversedWaveTangents = new Vector2[10]
-      {
-        new Vector2(-WaterVolume.s_createWaveDirections[0].y,
-          WaterVolume.s_createWaveDirections[0].x),
-        new Vector2(-WaterVolume.s_createWaveDirections[1].y,
-          WaterVolume.s_createWaveDirections[1].x),
-        new Vector2(-WaterVolume.s_createWaveDirections[2].y,
-          WaterVolume.s_createWaveDirections[2].x),
-        new Vector2(-WaterVolume.s_createWaveDirections[3].y,
-          WaterVolume.s_createWaveDirections[3].x),
-        new Vector2(-WaterVolume.s_createWaveDirections[4].y,
-          WaterVolume.s_createWaveDirections[4].x),
-        new Vector2(-WaterVolume.s_createWaveDirections[5].y,
-          WaterVolume.s_createWaveDirections[5].x),
-        new Vector2(-WaterVolume.s_createWaveDirections[6].y,
-          WaterVolume.s_createWaveDirections[6].x),
-        new Vector2(-WaterVolume.s_createWaveDirections[7].y,
-          WaterVolume.s_createWaveDirections[7].x),
-        new Vector2(-WaterVolume.s_createWaveDirections[8].y,
-          WaterVolume.s_createWaveDirections[8].x),
-        new Vector2(-WaterVolume.s_createWaveDirections[9].y,
-          WaterVolume.s_createWaveDirections[9].x)
-      };
-
-      reversedDepthDirections[0] =
-        new Vector2(wind.x, reversedDepthDirections[0].y);
-      reversedDepthDirections[0] =
-        new Vector2(reversedDepthDirections[0].x, wind.z);
-      reversedDepthDirections[0].Normalize();
-      WaterVolume.s_createWaveTangents[0].x =
-        -WaterVolume.s_createWaveDirections[0].y;
-      WaterVolume.s_createWaveTangents[0].y =
-        WaterVolume.s_createWaveDirections[0].x;
-      float num1 = Mathf.Lerp(0.0f, wind.w, depth);
-      float time = waterTime / 20f;
-      double wave1 = (double)__instance.CreateWave(worldPos, time, 10f, 0.04f,
-        8f,
-        reversedDepthDirections[0],
-        reversedWaveTangents[0], 0.5f);
-      float wave2 = __instance.CreateWave(worldPos, time, 14.123f, 0.08f, 6f,
-        reversedDepthDirections[1],
-        reversedWaveTangents[1], 0.5f);
-      float wave3 = __instance.CreateWave(worldPos, time, 22.312f, 0.1f, 4f,
-        reversedDepthDirections[2],
-        reversedWaveTangents[2], 0.5f);
-      float wave4 = __instance.CreateWave(worldPos, time, 31.42f, 0.2f, 2f,
-        reversedDepthDirections[3],
-        reversedWaveTangents[3], 0.5f);
-      float wave5 = __instance.CreateWave(worldPos, time, 35.42f, 0.4f, 1f,
-        reversedDepthDirections[4],
-        reversedWaveTangents[4], 0.5f);
-      float wave6 = __instance.CreateWave(worldPos, time, 38.1223f, 1f, 0.8f,
-        reversedDepthDirections[5],
-        reversedWaveTangents[5], 0.7f);
-      float wave7 = __instance.CreateWave(worldPos, time, 41.1223f, 1.2f,
-        0.6f * waveFactor, reversedDepthDirections[6],
-        reversedWaveTangents[6], 0.8f);
-      float wave8 = __instance.CreateWave(worldPos, time, 51.5123f, 1.3f,
-        0.4f * waveFactor, reversedDepthDirections[7],
-        reversedWaveTangents[7], 0.9f);
-      float wave9 = __instance.CreateWave(worldPos, time, 54.2f, 1.3f,
-        0.3f * waveFactor, reversedDepthDirections[8],
-        reversedWaveTangents[8], 0.9f);
-      float wave10 = __instance.CreateWave(worldPos, time, 56.123f, 1.5f,
-        0.2f * waveFactor, reversedDepthDirections[9],
-        reversedWaveTangents[9], 0.9f);
-      double num2 = (double)wave2;
-      var outputRes = ((float)(wave1 + num2) + wave3 + wave4 + wave5 + wave6 +
-                       wave7 +
-                       wave8 + wave9 + wave10) * num1;
-
-      // Call the original method to calculate the wave
-      __result = outputRes;
-      return false;
-    }
-
     [HarmonyPatch(typeof(WaterVolume), "UpdateMaterials")]
     [HarmonyPrefix]
     public static void WaterVolumeUpdatePatchWaterVolume(WaterVolume __instance,
@@ -217,11 +71,11 @@ namespace ValheimVehicles.Patches
     {
       if (WaterConfig.UnderwaterAccessMode.Value ==
           WaterConfig.UnderwaterAccessModeType.Disabled) return;
-      UpdateCameraWaterLevel(__instance);
+      UpdateWaterLevel(__instance);
       AdjustWaterSurface(__instance, ___m_normalizedDepth);
     }
 
-    private static void UpdateCameraWaterLevel(WaterVolume __instance)
+    private static void UpdateWaterLevel(WaterVolume __instance)
     {
       if (WaterConfig.UnderwaterAccessMode.Value ==
           WaterConfig.UnderwaterAccessModeType.Disabled) return;
@@ -230,10 +84,6 @@ namespace ValheimVehicles.Patches
         WaterLevelCamera =
           __instance.GetWaterSurface(GameCamera.instance.transform.position);
       }
-
-      var isCurrentCameraAboveWater =
-        GameCameraPatch.CameraPositionY > WaterLevelCamera;
-      UpdateCameraState(isCurrentCameraAboveWater);
     }
 
     [HarmonyPatch(typeof(WaterVolume), nameof(WaterVolume.CreateWave))]
@@ -288,6 +138,10 @@ namespace ValheimVehicles.Patches
       if (WaterConfig.UnderwaterAccessMode.Value ==
           WaterConfig.UnderwaterAccessModeType.Disabled) return;
 
+      var isCurrentCameraAboveWater =
+        GameCameraPatch.CameraPositionY > WaterLevelCamera;
+      UpdateCameraState(isCurrentCameraAboveWater);
+
       var waterSurfaceTransform = __instance.m_waterSurface.transform;
       var isCurrentlyFlipped =
         waterSurfaceTransform.rotation.eulerAngles.y.Equals(180f);
@@ -299,14 +153,14 @@ namespace ValheimVehicles.Patches
         if (!isCurrentlyFlipped)
         {
           FlipWaterSurface(__instance, normalizedDepth);
-          // SetWaterSurfacePosition(waterSurfaceTransform, WaterLevelCamera);
+          SetWaterSurfacePosition(waterSurfaceTransform, WaterLevelCamera);
         }
 
         if (isCurrentlyFlipped)
         {
           UnflipWaterSurface(__instance, normalizedDepth);
-          // SetWaterSurfacePosition(waterSurfaceTransform,
-          //   WaterLevelCamera);
+          SetWaterSurfacePosition(waterSurfaceTransform,
+            WaterLevelCamera);
         }
       }
     }
@@ -344,34 +198,27 @@ namespace ValheimVehicles.Patches
     private static void SetDepthForWaterSurface(WaterVolume __instance,
       float[] normalizedDepth, bool isFlipped)
     {
-      // x flip swaps 1,1 to 0,1 IE indexes [0] [1] swap
-      // x flip swaps 0,0 to 1,0 IE indexes [2] [3] swap
-      float[] oceanDepth = __instance.m_heightmap.GetOceanDepth();
       var depthValues = isFlipped
         ?
         [
           //     // normalizedDepth[0],
           //     // normalizedDepth[0],
           //     // normalizedDepth[0],
-          1 - normalizedDepth[1],
-          1 - normalizedDepth[0],
-          1 - normalizedDepth[3],
-          1 - normalizedDepth[2],
-          // normalizedDepth[1],
-          // 1 - normalizedDepth[0],
-          // 1 - normalizedDepth[1],
-          // 1 - normalizedDepth[2],
-          // 1 - normalizedDepth[3]
-          // -normalizedDepth[0],
-          // -normalizedDepth[1],
-          // -normalizedDepth[2],
-          // -normalizedDepth[3]
+          //     // normalizedDepth[0]
+          //     // normalizedDepth[2],
+          //     // normalizedDepth[0],
+          //     // normalizedDepth[3],
+          //     // normalizedDepth[1],
+          //     // 1 - normalizedDepth[0],
+          //     // 1 - normalizedDepth[1],
+          //     // 1 - normalizedDepth[2],
+          //     // 1 - normalizedDepth[3]
+          -normalizedDepth[0],
+          -normalizedDepth[1],
+          -normalizedDepth[2],
+          -normalizedDepth[3]
         ]
         : normalizedDepth;
-      __instance.m_normalizedDepth[0] = depthValues[0];
-      __instance.m_normalizedDepth[1] = depthValues[1];
-      __instance.m_normalizedDepth[2] = depthValues[2];
-      __instance.m_normalizedDepth[3] = depthValues[3];
 
       __instance.m_waterSurface.material.SetFloatArray(DepthProperty
         , depthValues);
