cab2df894303e65831cd78b08957de2a6c61ef5e
zolantris <zolantris@vllc.dev>
2024-11-09 23:45:12 -0800

    determine if it's easy to enable ship markers for all areas of map

--- src/ValheimVehicles/ValheimVehicles.Patches/Minimap_VehicleIcons.cs
+++ src/ValheimVehicles/ValheimVehicles.Patches/Minimap_VehicleIcons.cs
@@ -1,5 +1,6 @@
 using System;
 using HarmonyLib;
+using UnityEngine;
 
 namespace ValheimVehicles.Patches;
 
@@ -17,4 +18,52 @@ public class Minimap_VehicleIcons
   //
   //   return;
   // }
+  [HarmonyPatch(typeof(Minimap), nameof(Minimap.UpdatePlayerMarker))]
+  [HarmonyPrefix]
+  public static bool UpdatePlayerMarker(Minimap __instance, Player player,
+    Quaternion playerRot)
+  {
+    Vector3 position = player.transform.position;
+    __instance.m_smallMarker.rotation =
+      Quaternion.Euler(0.0f, 0.0f, -playerRot.eulerAngles.y);
+    if (__instance.m_mode == Minimap.MapMode.Large &&
+        __instance.IsPointVisible(position, __instance.m_mapImageLarge))
+    {
+      __instance.m_largeMarker.gameObject.SetActive(true);
+      __instance.m_largeMarker.rotation = __instance.m_smallMarker.rotation;
+      float mx;
+      float my;
+      __instance.WorldToMapPoint(position, out mx, out my);
+      __instance.m_largeMarker.anchoredPosition =
+        __instance.MapPointToLocalGuiPos(mx, my, __instance.m_mapImageLarge);
+    }
+    else
+      __instance.m_largeMarker.gameObject.SetActive(false);
+
+    Ship controlledShip = player.GetControlledShip();
+    if ((bool)(UnityEngine.Object)controlledShip)
+    {
+      __instance.m_smallShipMarker.gameObject.SetActive(true);
+      __instance.m_smallShipMarker.rotation = Quaternion.Euler(0.0f, 0.0f,
+        -controlledShip.transform.rotation.eulerAngles.y);
+      if (__instance.m_mode != Minimap.MapMode.Large)
+        return;
+      __instance.m_largeShipMarker.gameObject.SetActive(true);
+      float mx;
+      float my;
+      __instance.WorldToMapPoint(controlledShip.transform.position, out mx,
+        out my);
+      __instance.m_largeShipMarker.anchoredPosition =
+        __instance.MapPointToLocalGuiPos(mx, my, __instance.m_mapImageLarge);
+      __instance.m_largeShipMarker.rotation =
+        __instance.m_smallShipMarker.rotation;
+    }
+    else
+    {
+      __instance.m_smallShipMarker.gameObject.SetActive(false);
+      __instance.m_largeShipMarker.gameObject.SetActive(false);
+    }
+
+    return false;
+  }
 }
