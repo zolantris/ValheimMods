058a06d3712e748b102098c4eff459b5b58a3b4c
zolantris <zolantris@vllc.dev>
2025-03-18 20:29:03 -0700

    WIP add jobdata and broken convexhulljob, this can be reverted later.

---
+++ src/ValheimRAFT.Unity/Assets/ValheimVehicles/SharedScripts/JobData.cs
@@ -0,0 +1,35 @@
+﻿// ReSharper disable ArrangeNamespaceBody
+// ReSharper disable NamespaceStyle
+
+#region
+
+using System;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+#endregion
+
+namespace ValheimVehicles.SharedScripts
+{
+// Struct to track job-related native arrays and cleanup
+  public struct JobData
+  {
+    public JobHandle Handle;
+    public NativeArray<int> ValidClusterCount;
+    public NativeArray<Vector3> OutputVertices;
+    public NativeArray<int> OutputTriangles;
+    public NativeArray<Vector3> OutputNormals;
+    public NativeArray<PrefabColliderPointData> ColliderDataArray;
+    public Action OnComplete;
+
+    public void Dispose()
+    {
+      if (ValidClusterCount.IsCreated) ValidClusterCount.Dispose();
+      if (OutputVertices.IsCreated) OutputVertices.Dispose();
+      if (OutputTriangles.IsCreated) OutputTriangles.Dispose();
+      if (OutputNormals.IsCreated) OutputNormals.Dispose();
+      if (ColliderDataArray.IsCreated) ColliderDataArray.Dispose();
+    }
+  }
+}
