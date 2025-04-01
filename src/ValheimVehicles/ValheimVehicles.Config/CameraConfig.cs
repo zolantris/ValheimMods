f8289425730a3ef52463319e4af82b1b5e75f9bf
zolantris <zolantris@vllc.dev>
2025-04-01 02:38:05 -0700

    - rename camera zoom to vehicleCameraZoom so it's more clear that users can zoom out with it - set DistanceToKeepObjects with UNSTABLE flag.

--- src/ValheimVehicles/ValheimVehicles.Config/CameraConfig.cs
+++ src/ValheimVehicles/ValheimVehicles.Config/CameraConfig.cs
@@ -21,7 +21,7 @@ public static class CameraConfig
   public static ConfigEntry<float> DistanceToKeepObjects = null!;
 
   // zoom
-  public static ConfigEntry<float> CameraZoomMaxDistance = null!;
+  public static ConfigEntry<float> VehicleCameraZoomMaxDistance = null!;
 
   /// <summary>
   /// Debug will always run this. However onboard controller should control this normally.
@@ -29,7 +29,7 @@ public static class CameraConfig
   public static void OnCameraZoomChange()
   {
 #if DEBUG
-    if (!CameraZoomOverridesEnabled.Value || CameraZoomMaxDistance.Value == 0)
+    if (!CameraZoomOverridesEnabled.Value || VehicleCameraZoomMaxDistance.Value == 0)
     {
       return;
     }
@@ -42,7 +42,7 @@ public static class CameraConfig
     var mainCamera = Camera.main.GetComponent<GameCamera>();
     if (!mainCamera) return;
 
-    mainCamera.m_maxDistance = Mathf.Lerp(cameraZoomMultiplier, Mathf.Pow(cameraZoomMultiplier, 2), CameraZoomMaxDistance.Value);
+    mainCamera.m_maxDistance = Mathf.Lerp(cameraZoomMultiplier, Mathf.Pow(cameraZoomMultiplier, 2), VehicleCameraZoomMaxDistance.Value);
 #endif
   }
 
@@ -61,23 +61,23 @@ public static class CameraConfig
         false, false));
 
     DistanceToKeepObjects = Config.Bind(CameraOptimizationsKey,
-      "DistanceToKeepObjects", 5f,
+      "UNSTABLE_DistanceToKeepObjects", 5f,
       ConfigHelpers.CreateConfigDescription(
         $"Threshold at which to retain a object even if it's through a wall.",
         false, false, new AcceptableValueRange<float>(0, 20f)));
 
     CameraZoomOverridesEnabled = Config.Bind(CameraZoomKey,
-      "CameraZoom Enabled", false, ConfigHelpers.CreateConfigDescription(
+      "VehicleCameraZoom_Enabled", false, ConfigHelpers.CreateConfigDescription(
         $"Overrides the camera zoom while on the vehicle. Values are configured through other keys.",
         true));
-    CameraZoomMaxDistance = config.Bind(CameraZoomKey,
-      "CameraZoomMaxDistance",
-      0f,
+    VehicleCameraZoomMaxDistance = config.Bind(CameraZoomKey,
+      "VehicleCameraZoomMaxDistance",
+      0.5f,
       ConfigHelpers.CreateConfigDescription(
         "Allows the camera to zoom out between 8 and 64 meters. Percentage based zoom.",
         true, true, new AcceptableValueRange<float>(0f, 1f)));
 
-    CameraZoomMaxDistance.SettingChanged +=
+    VehicleCameraZoomMaxDistance.SettingChanged +=
       (_, _) => OnCameraZoomChange();
     CameraOcclusionInterval.SettingChanged += (sender, args) =>
       VehicleCameraCullingComponent.AddOrRemoveCameraCulling();
