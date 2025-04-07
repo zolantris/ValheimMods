139385ea94319067ca6a9b1180269ebc15a19b15
zolantris <zolantris@vllc.dev>
2025-04-07 06:36:21 -0700

    Fix hammer alignment with unity. Hammer was not aligned correcty due to attach not being 90 rotation

--- src/ValheimVehicles/ValheimVehicles.Prefabs/ValheimVehicles.Prefabs.Registry/VehicleHammerItemRegistry.cs
+++ src/ValheimVehicles/ValheimVehicles.Prefabs/ValheimVehicles.Prefabs.Registry/VehicleHammerItemRegistry.cs
@@ -2,13 +2,24 @@ using Jotunn.Configs;
 using Jotunn.Entities;
 using Jotunn.Managers;
 using UnityEngine;
+using Valheim.UI;
 using ValheimVehicles.SharedScripts;
 namespace ValheimVehicles.Prefabs.Registry;
 
-public class VehicleHammerItemRegistry : GuardedRegistry<VehicleHammerTableRegistry>
+public class VehicleHammerItemRegistry : GuardedRegistry<VehicleHammerItemRegistry>
 {
-  public class VehicleHammer : MonoBehaviour
+  public class VehicleBuildHammer : MonoBehaviour, Interactable
   {
+    public bool Interact(Humanoid user, bool hold, bool alt)
+    {
+      LoggerProvider.LogMessage("Interact called for vehicle hammer");
+      return false;
+    }
+    public bool UseItem(Humanoid user, ItemDrop.ItemData item)
+    {
+      LoggerProvider.LogMessage("Called UseItem for vehicle hammer");
+      return false;
+    }
   }
 
   public static void RegisterVehicleHammer()
@@ -22,6 +33,15 @@ public class VehicleHammerItemRegistry : GuardedRegistry<VehicleHammerTableRegis
     }
 
     var nv = PrefabRegistryHelpers.AddNetViewWithPersistence(hammerPrefab);
+    var zSyncTransform = hammerPrefab.AddComponent<ZSyncTransform>();
+
+    hammerPrefab.AddComponent<VehicleBuildHammer>();
+
+    // verbosely add these.
+    zSyncTransform.m_syncBodyVelocity = false;
+    zSyncTransform.m_syncRotation = true;
+    zSyncTransform.m_syncPosition = true;
+    
     var itemDrop = hammerPrefab.AddComponent<ItemDrop>();
     if (itemDrop.m_nview == null)
     {
@@ -33,21 +53,47 @@ public class VehicleHammerItemRegistry : GuardedRegistry<VehicleHammerTableRegis
     itemDrop.m_itemData.m_shared = new ItemDrop.ItemData.SharedData
     {
       m_name = PrefabNames.VehicleHammer,
+      m_maxQuality = 5,
       m_useDurability = true,
+      m_useDurabilityDrain = 1f,
       m_durabilityDrain = 0f,
       m_durabilityPerLevel = 200f,
+      m_maxDurability = 100f,
       m_icons = [icon],
       m_buildPieces = PrefabRegistryController.GetPieceTable(),
-      m_toolTier = 1,
+      m_toolTier = 0,
+      m_animationState = ItemDrop.ItemData.AnimationState.OneHanded,
+      // must be 0 to override default of 1. 1 adds an equip delay automatically
+      m_equipDuration = 0,
       // See In HammerItemElement.IsHammer (valheim.dll) regarding hammer logic matchers
       // requires
       // - m_skillType =  Skills.SkillType.Swords
       // - m_itemType = ItemDrop.ItemData.ItemType.Tool
       // - prefab name must container "hammer" lower case.
-      m_equipDuration = 0,
       m_skillType = Skills.SkillType.Swords,
       m_itemType = ItemDrop.ItemData.ItemType.Tool
     };
+
+    if (itemDrop.m_itemData.m_shared.m_attack == null)
+    {
+      itemDrop.m_itemData.m_shared.m_attack = new Attack();
+    }
+
+    if (itemDrop.m_itemData.m_shared.m_secondaryAttack == null)
+    {
+      itemDrop.m_itemData.m_shared.m_secondaryAttack = new Attack();
+    }
+
+    itemDrop.m_itemData.m_shared.m_attack.m_attackAnimation = "swing_hammer";
+    itemDrop.m_itemData.m_shared.m_attack.m_attackType = Attack.AttackType.Horizontal;
+    itemDrop.m_itemData.m_shared.m_attack.m_attackStamina = 5;
+
+    // itemDrop.m_itemData.m_shared.m_secondaryAttack.m_attackAnimation = "";
+    itemDrop.m_itemData.m_shared.m_secondaryAttack.m_attackType = Attack.AttackType.Horizontal;
+    itemDrop.m_itemData.m_shared.m_secondaryAttack.m_attackAnimation = "swing_hammer";
+    itemDrop.m_itemData.m_shared.m_secondaryAttack.m_attackStamina = 20;
+    // itemDrop.m_itemData.m_shared.m_attack.m_speedFactor = 0.2f; // default value. can remove 
+    // itemDrop.m_itemData.m_shared.m_attack.m_speedFactorRotation = 0.2f; // default value. can remove.
 
     var itemConfig = new ItemConfig
     {
