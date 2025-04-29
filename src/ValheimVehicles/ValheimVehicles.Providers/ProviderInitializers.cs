ba86a4e5779a563bab83f2768dac41bf709246b8
zolantris <zolantris@vllc.dev>
2025-04-28 21:05:20 -0700

    - Fix translation issues where Chinese was resolved when english was selected due to how callbacks are done. Needed to add a delay after instance was available - cleanup ModTranslation logic. Add more helpers - Add coroutine for VehiclesPlugin to wait for next frame on Translation   calls.

--- src/ValheimVehicles/ValheimVehicles.Providers/ProviderInitializers.cs
+++ src/ValheimVehicles/ValheimVehicles.Providers/ProviderInitializers.cs
@@ -1,6 +1,8 @@
 using BepInEx;
 using BepInEx.Logging;
+using UnityEngine;
 using ValheimVehicles.SharedScripts;
+using Zolantris.Shared;
 namespace ValheimVehicles.Providers;
 
 public static class ProviderInitializers
@@ -12,9 +14,14 @@ public static class ProviderInitializers
   /// <note>
   /// This is a new pattern to ensure this Mod can test code in isolation but still reference valheim code at runtime
   /// </note>
-  public static void InitProviders(ManualLogSource Logger)
+  public static void InitProviders(ManualLogSource Logger, GameObject gameObject)
   {
     new WearNTearIntegrationProvider().Init();
     LoggerProvider.Setup(Logger);
+
+#if DEBUG
+    // todo inject LoggerProvider into BatchedLogger.
+    // gameObject.AddComponent<BatchedLogger>();
+#endif
   }
 }
