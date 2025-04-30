cea58c70961b85cd7b3136ffbbc147cce15bab46
zolantris <zolantris@vllc.dev>
2025-04-30 02:03:58 -0700

    add observableField which will be used for style updates during playmode

---
+++ src/ValheimRAFT.Unity/Assets/ValheimVehicles/SharedScripts/ObservableField.cs
@@ -0,0 +1,39 @@
+﻿#region
+
+using System;
+
+#endregion
+
+namespace ValheimVehicles.SharedScripts.UI.Observables
+{
+  [Serializable]
+  public class ObservableField<T>
+  {
+    private T _value;
+
+    public ObservableField(T initial)
+    {
+      _value = initial;
+    }
+
+    public T Value
+    {
+      get => _value;
+      set
+      {
+        if (!Equals(_value, value))
+        {
+          _value = value;
+          OnValueChanged?.Invoke(_value);
+        }
+      }
+    }
+
+    public event Action<T> OnValueChanged;
+
+    public void ForceNotify()
+    {
+      OnValueChanged?.Invoke(_value);
+    }
+  }
+}
