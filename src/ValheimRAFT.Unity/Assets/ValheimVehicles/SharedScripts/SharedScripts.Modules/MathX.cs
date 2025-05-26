639feb1f7e9db0c29f2d10aab8e0c1824fe670fc
zolantris <zolantris@vllc.dev>
2025-05-26 12:21:04 -0700

    Fix compilation flag issues for testing files

--- src/ValheimRAFT.Unity/Assets/ValheimVehicles/SharedScripts/SharedScripts.Modules/MathX.cs
+++ src/ValheimRAFT.Unity/Assets/ValheimVehicles/SharedScripts/SharedScripts.Modules/MathX.cs
@@ -52,40 +52,29 @@ namespace ValheimVehicles.SharedScripts.Modules
     }
   }
 #else
-/// <summary>
-/// For mocking. This will switch over to UnityEngine Mathf in release builds.
-/// </summary>
-public static class MathX // or MathMock, MathUtil, etc.
-{
-  public static float Clamp(float v, float min, float max)
+  /// <summary>
+  /// For mocking. This will switch over to UnityEngine Mathf in release builds.
+  /// </summary>
+  public static partial class MathX // or MathMock, MathUtil, etc.
   {
-    return Math.Max(min, Math.Min(max, v));
-  }
-  public static float Min(float a, float b)
-  {
-    return Math.Min(a, b);
-  }
+    public static float Clamp(float v, float min, float max)
+    {
+      return Math.Max(min, Math.Min(max, v));
+    }
+    public static float Min(float a, float b)
+    {
+      return Math.Min(a, b);
+    }
 
-  public static float Min(params float[] values)
-  {
-    if (values == null || values.Length == 0)
-      throw new ArgumentException("No values supplied to MathX.Min");
+    public static float Max(float a, float b)
+    {
+      return Math.Max(a, b);
+    }
 
-    var min = values[0];
-    for (var i = 1; i < values.Length; i++)
-      min = Min(min, values[i]);
-    return min;
+    public static float Round(float f)
+    {
+      return (float)Math.Round(f, MidpointRounding.AwayFromZero);
+    }
   }
-
-  public static float Max(float a, float b)
-  {
-    return Math.Max(a, b);
-  }
-
-  public static float Round(float f)
-  {
-    return (float)Math.Round(f, MidpointRounding.AwayFromZero);
-  }
-}
 #endif
 }
