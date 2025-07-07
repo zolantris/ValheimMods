dcfc00a98e307ff460a1030f10e45727b12efbdf
zolantris <zolantris@vllc.dev>
2025-07-07 10:21:57 -0700

    Fix asset name

--- src/ValheimVehicles/ValheimVehicles.Prefabs/ValheimVehicles.Prefabs.Loading/LoadValheimVehiclesAssets.cs
+++ src/ValheimVehicles/ValheimVehicles.Prefabs/ValheimVehicles.Prefabs.Loading/LoadValheimVehiclesAssets.cs
@@ -389,7 +389,7 @@ public class LoadValheimVehicleAssets : ILoadAssets
     RuntimeDebugLineDrawer.DebugRayMaterial = new Material(DoubleSidedTransparentMat);
 
     // for hit effect with solid cannonball.
-    Cannonball.ImpactSoundOverride = assetBundle.LoadAsset<AudioClip>("impact-hit-sound-fx");
+    Cannonball.ImpactSoundOverride = assetBundle.LoadAsset<AudioClip>("impact-hit-sound-fx.mp3");
 
     PowerPylon.LightningMaterial = LightningMaterial;
     MovingTreadComponent.fallbackPrefab = TankTreadsSingle;
