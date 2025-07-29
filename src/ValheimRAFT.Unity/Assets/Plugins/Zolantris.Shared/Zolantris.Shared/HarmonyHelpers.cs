0017b97a4235e21f2fa53d87ab9e679577000173
zolantris <zolantris@vllc.dev>
2025-07-28 21:43:24 -0700

    WIP: symlink Shared to Plugins folder for Zolantris.Shared and Eldritch.Core

---
+++ src/ValheimRAFT.Unity/Assets/Plugins/Zolantris.Shared/Zolantris.Shared/HarmonyHelpers.cs
@@ -0,0 +1,40 @@
+using System;
+using HarmonyLib;
+namespace Zolantris.Shared;
+
+public static class HarmonyHelper
+{
+#if DEBUG
+  private const bool IsDebug = true;
+#else
+  private const bool IsDebug = false;
+#endif
+  /// <summary>
+  /// Safely patches all annotated methods in the specified class. This will ensure the mod does not
+  /// </summary>
+  /// <param name="harmonyInstance">Harmony instance.</param>
+  /// <param name="type">Class containing Harmony patch methods.</param>
+  public static void TryPatchAll(Harmony harmonyInstance, Type type)
+  {
+    try
+    {
+      harmonyInstance.PatchAll(type);
+      if (IsDebug)
+      {
+        LoggerProvider.LogDebug($"[Harmony] Successfully patched: {type.Name}");
+      }
+    }
+    catch (Exception ex)
+    {
+      LoggerProvider.LogError($"[Harmony] Failed to patch: {type.Name}\n{ex}");
+    }
+  }
+
+  public static void TryPatchAll(Harmony harmonyInstance, params Type[] patchTypes)
+  {
+    foreach (var type in patchTypes)
+    {
+      TryPatchAll(harmonyInstance, type);
+    }
+  }
+}
