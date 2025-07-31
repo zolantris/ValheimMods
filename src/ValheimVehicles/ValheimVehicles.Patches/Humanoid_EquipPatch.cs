b66ea04e8d2a8a93d36583c4eeafe6562d9cd3b1
zolantris <zolantris@vllc.dev>
2025-07-30 22:34:31 -0700

    add more NRE guards for equippatches

--- src/ValheimVehicles/ValheimVehicles.Patches/Humanoid_EquipPatch.cs
+++ src/ValheimVehicles/ValheimVehicles.Patches/Humanoid_EquipPatch.cs
@@ -14,30 +14,46 @@ public static class Humanoid_EquipPatch
   // must use translation names for shared.m_name
   public static Dictionary<Player, CannonHandHeldController> PlayerCannonController = new();
 
-  [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.EquipItem), new Type[] { typeof(ItemDrop.ItemData), typeof(bool) })]
+  // [HarmonyPatch(typeof(Humanoid), nameof(EquipItem), new Type[] { typeof(ItemDrop.ItemData), typeof(bool) })]
+  [HarmonyPatch(typeof(Humanoid), "EquipItem", new Type[] { typeof(ItemDrop.ItemData), typeof(bool) })]
   [HarmonyPostfix]
   private static void EquipItemPatch(Player __instance, ItemDrop.ItemData item, bool triggerEquipEffects)
   {
-    if (__instance == null) return;
-    if (item.m_shared.m_name != PrefabItemNameToken.CannonHandHeldName) return;
-    CannonPrefabConfig.SyncHandheldItemData(item);
+    try
+    {
+      if (__instance == null) return;
+      if (item == null) return;
+      if (item.m_shared == null) return;
+      if (item.m_shared.m_name != PrefabItemNameToken.CannonHandHeldName) return;
+      CannonPrefabConfig.SyncHandheldItemData(item);
+    }
+    catch (Exception e)
+    {
+      LoggerProvider.LogDebugDebounced($"Problem occurred with EquipItemPatch. Report this issue. {e}");
+    }
   }
 
-  [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.UnequipItem), new Type[] { typeof(ItemDrop.ItemData), typeof(bool) })]
+  // [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.UnequipItem), new Type[] { typeof(ItemDrop.ItemData), typeof(bool) })]
+  [HarmonyPatch(typeof(Humanoid), "UnequipItem", new Type[] { typeof(ItemDrop.ItemData), typeof(bool) })]
   [HarmonyPostfix]
   private static void UnequipItemPatch(Player __instance, ItemDrop.ItemData item, bool triggerEquipEffects)
   {
     if (item == null || __instance == null) return;
+    if (item.m_shared == null) return;
     if (item.m_shared.m_name != PrefabItemNameToken.CannonHandHeldName) return;
     PlayerCannonController.Remove(__instance);
     PlayerCannonController.RemoveNullKeys();
   }
 
 
-  [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.OnAttackTrigger))]
+  // [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.OnAttackTrigger))]
+  [HarmonyPatch(typeof(Humanoid), "OnAttackTrigger")]
   [HarmonyPrefix]
   private static bool HandCannon_OnAttackTrigger(Player __instance)
   {
+    if (__instance == null) return false;
+    if (__instance.IsDead()) return false;
+
     var currentWeapon = __instance.GetCurrentWeapon();
     if (currentWeapon == null || currentWeapon.m_shared.m_name != PrefabItemNameToken.CannonHandHeldName) return true;
     if (!PlayerCannonController.TryGetValue(__instance, out var cannonHandheldController) || cannonHandheldController == null)
