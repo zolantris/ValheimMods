f7d10d677703fb3d8e8d4acfa10dbc8d8a854ee3
zolantris <zolantris@vllc.dev>
2025-12-03 19:30:53 -0800

    Make cannon prefixes higher priority and ensure that there is a final prefix that will bail on original action if it was a cannon and handled by the higher priority prefix

--- src/ValheimVehicles/ValheimVehicles.Patches/Humanoid_EquipPatch.cs
+++ src/ValheimVehicles/ValheimVehicles.Patches/Humanoid_EquipPatch.cs
@@ -43,14 +43,25 @@ public static class Humanoid_EquipPatch
     PlayerCannonController.RemoveNullKeys();
   }
 
+  public static bool _HandCannonWasHandled = false;
+  // todo determine if this is needed for flexibility.
+  public static bool AllowOtherPrefixesToApplyToHandheldCannon = false;
 
-  // [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.OnAttackTrigger))]
-  [HarmonyPatch(typeof(Humanoid), "OnAttackTrigger")]
+  // We need a prefix otherwise other patches could run and fire the cannon or interfere with the cannon logic. This must be run first.
   [HarmonyPrefix]
+  [HarmonyPriority(Priority.VeryHigh)] // This patch will run before others with lower priority
+#if DEBUG
+  [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.OnAttackTrigger))]
+#else
+  [HarmonyPatch(typeof(Humanoid), "OnAttackTrigger")]
+#endif
   private static bool HandCannon_OnAttackTrigger(Player __instance)
   {
-    if (__instance == null) return false;
-    if (__instance.IsDead()) return false;
+    // resets it always.
+    _HandCannonWasHandled = false;
+
+    if (__instance == null) return true;
+    if (__instance.IsDead()) return true;
 
     var currentWeapon = __instance.GetCurrentWeapon();
     if (currentWeapon == null || currentWeapon.m_shared.m_name != PrefabItemNameToken.CannonHandHeldName) return true;
@@ -71,14 +82,57 @@ public static class Humanoid_EquipPatch
 
     CannonPrefabConfig.SyncHandheldItemData(currentWeapon);
 
+    _HandCannonWasHandled = true;
+
     var ammoItem = __instance.GetAmmoItem();
+
     if (ammoItem == null)
     {
-      return false;
+      if (AllowOtherPrefixesToApplyToHandheldCannon)
+      {
+        return true;
+      }
+      else
+      {
+        // no prefixes run after this one.
+        return false;
+      }
     }
+
     cannonHandheldController.SetAmmoVariantFromToken(ammoItem.m_shared.m_name);
     cannonHandheldController.Request_FireHandHeld();
 
-    return false;
+    if (AllowOtherPrefixesToApplyToHandheldCannon)
+    {
+      return true;
+    }
+    else
+    {
+      // no prefixes run after this one.
+      return false;
+    }
+  }
+
+  /// <summary>
+  /// This prefix only runs if we allow additional OnAttackTrigger prefixes to run for the cannon. Otherwise it will just return true and allow original methods to fire for OnAttackTrigger.
+  /// </summary>
+  /// <param name="__instance"></param>
+  /// <returns></returns>
+  [HarmonyPrefix]
+  [HarmonyPriority(Priority.Last)] // This patch will run after others with lower priority and will only bail if the first prefix was handled.
+#if DEBUG
+  [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.OnAttackTrigger))]
+#else
+  [HarmonyPatch(typeof(Humanoid), "OnAttackTrigger")]
+#endif
+  private static bool HandCannon_OnAttackTriggerShouldBail(Player __instance)
+  {
+    if (_HandCannonWasHandled)
+    {
+      _HandCannonWasHandled = false;
+      return false;
+    }
+
+    return true;
   }
 }
